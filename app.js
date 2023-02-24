94a2a7d23cb30854235b78c516224f159dfa9ab7
Aurelien Briant <aurelienbriant@macbook-air-2.home>
2023-02-24 17:18:37 +0100

    variable port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Confirming");
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome to the homepage");
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log("Confirmed");
 });
